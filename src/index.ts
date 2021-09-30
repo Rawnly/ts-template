b720901183d51b05a73eb3361e3564665eabc8f4
Rawnly <fedevitale99@gmail.com>
2021-09-30 16:58:50 +0200

    build(esbuild): removed `tsc` and added `esbuild`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export const sum = ( ...numbers: number[] ): number =>
   numbers.reduce( ( a, b ) => a + b, 0 )
+
+export const multiply = ( ...numbers: number[] ): number =>
+  numbers.reduce( ( a, b ) => a * b, 1 )
